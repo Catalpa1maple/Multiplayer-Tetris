c87ee6303526b66f8ca22d4d9cafbe4df00be46d
Jimmy Wu <jay18859@gmail.com>
2024-12-22 01:03:28 +0800

    setupgamecanvas

--- Tetris/MainWindow.xaml.cs
+++ Tetris/MainWindow.xaml.cs
@@ -35,7 +35,25 @@ namespace Tetris
         private Image[,] SetupGameCanvas(GameGrid grid)
         {
             Image[,] imageControls = new Image[grid.Rows, grid.Coluns];
-            int cellSize
+            int cellSize=25;
+
+            for (int r=0; r < grid.Rows; r++)
+            {
+                for (int c=0; c < grid.Columns; c++)
+                {
+                    Image imageControl = new Image
+                    {
+                        Width= cellSize,
+                        Height = cellSize
+                    };
+                    Canvas.SetTop(imageControl, (r-2)* cellSize);
+                    Canvas.SetLeft(imageControl, c* cellSize);
+                    GameCanvas.Children.Add(imageControl);
+                    imageControls[r,c]= imageControl;
+                }
+                
+            }
+            return imageControls;
         }
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
