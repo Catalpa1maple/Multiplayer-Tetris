dae1e030c478824b83529d0928f4a645a46a26ce
sembreakproduct <1155191897@link.cuhk.edu.hk>
2024-12-23 02:39:53 +0800

    sdv

--- Tetris/Position.cs
+++ Tetris/Position.cs
@@ -2,13 +2,70 @@ namespace Tetris
 {
     public class Position
     {
-        public int Row { get; set; }
-        public int Column { get; set; }
+        private int rowValue;
+        private int columnValue;
+        public int Row { 
+            get{
+                int temp = rowValue;
+                if (temp < 0)
+                {
+                    temp = 0; 
+                }
+                return temp;
+            }
+            set{
+                if (value < 0)
+                {
+                    rowValue = 0;
+                }
+                else
+                {
+                    rowValue = value;
+                }
+            }
+        }
+        public int Column { 
+            get
+            {
+                int temp = columnValue;
+                if (temp < 0)
+                {
+                    temp = 0;
+                }
+                return temp;
+            }
+        
+         set
+         {
+               
+                if (value < 0)
+                {
+                    columnValue = 0;
+                }
+                else
+                {
+                    columnValue = value;
+                }
+            }
+            }
 
         public Position(int row, int column)
         {
-            Row = row;
-            Column = column;
+            rowValue = row;
+            columnValue = column;
+            ValidatePosition();
+        }
+        private void ValidatePosition()
+        {
+            if (rowValue < 0)
+            {
+                rowValue = 0;
+            }
+
+            if (columnValue < 0)
+            {
+                columnValue = 0;
+            }
         }
     }
 }
