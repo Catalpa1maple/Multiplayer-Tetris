aace6bed9cb554eb63f0c65793611363f82e701b
sembreakproduct <1155191897@link.cuhk.edu.hk>
2024-12-23 03:08:56 +0800

    sdgv

--- Tetris/I-Block.cs
+++ Tetris/I-Block.cs
@@ -1,23 +1,17 @@
-namespace Tetris{
-    // Represents the "I" shaped block in Tetris.
+namespace Tetris
+{
     public class IBlock : Block
     {
-        // Defines the tile positions for the four rotation states of the "I" block.
-        // Each array represents a rotation state, and the positions are relative to the block's offset.
-        private readonly Position[][] tiles = new Position[][]{
-            new Position[] { new(1, 0), new(1, 1), new(1, 2), new(1, 3) }, // Horizontal
-            new Position[] { new(0, 2), new(1, 2), new(2, 2), new(3, 2) }, // Vertical
-            new Position[] { new(2, 0), new(2, 1), new(2, 2), new(2, 3) }, // Horizontal (flipped)
-            new Position[] { new(0, 1), new(1, 1), new(2, 1), new(3, 1) }  // Vertical (flipped)
+        private readonly Position[][] tiles = new Position[][]
+        {
+            new Position[] { new(1,0), new(1,1), new(1,2), new(1,3) },
+            new Position[] { new(0,2), new(1,2), new(2,2), new(3,2) },
+            new Position[] { new(2,0), new(2,1), new(2,2), new(2,3) },
+            new Position[] { new(0,1), new(1,1), new(2,1), new(3,1) }
         };
 
-        // Unique identifier for the "I" block type.
         public override int Id => 1;
-
-        // Defines the starting offset for the "I" block when it spawns on the grid.
         protected override Position StartOffset => new Position(-1, 3);
-
-        // Provides access to the rotation states of the "I" block.
         protected override Position[][] Tiles => tiles;
     }
 }
--- Tetris/Position.cs
+++ Tetris/Position.cs
@@ -2,70 +2,13 @@ namespace Tetris
 {
     public class Position
     {
-        private int rowValue;
-        private int columnValue;
-        public int Row { 
-            get{
-                int temp = rowValue;
-                if (temp < 0)
-                {
-                    temp = 0; 
-                }
-                return temp;
-            }
-            set{
-                if (value < 0)
-                {
-                    rowValue = 0;
-                }
-                else
-                {
-                    rowValue = value;
-                }
-            }
-        }
-        public int Column { 
-            get
-            {
-                int temp = columnValue;
-                if (temp < 0)
-                {
-                    temp = 0;
-                }
-                return temp;
-            }
-        
-         set
-         {
-               
-                if (value < 0)
-                {
-                    columnValue = 0;
-                }
-                else
-                {
-                    columnValue = value;
-                }
-            }
-            }
+        public int Row { get; set; }
+        public int Column { get; set; }
 
         public Position(int row, int column)
         {
-            rowValue = row;
-            columnValue = column;
-            ValidatePosition();
-        }
-        private void ValidatePosition()
-        {
-            if (rowValue < 0)
-            {
-                rowValue = 0;
-            }
-
-            if (columnValue < 0)
-            {
-                columnValue = 0;
-            }
+            Row = row;
+            Column = column;
         }
     }
 }
