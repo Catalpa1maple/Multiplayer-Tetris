93b994c07181bd7eba34e6fd4b1b2a4a3efdcf76
sembreakproduct <1155191897@link.cuhk.edu.hk>
2024-12-23 01:14:20 +0800

    efdc

--- Tetris/Z-block.cs
+++ Tetris/Z-block.cs
@@ -1,7 +1,7 @@
 namespace Tetris
 { 
     public class ZBlock: Block{
-        private readonly Position[][] Tiles => new Position[][]{
+        protected override Position[][] Tiles => new Position[][]{
             new Position[]{new(0,0), new(0,1), new(1,1), new(1,2)},
             new Position[]{new(0,2), new(1,1), new(1,2), new(2,1)},
             new Position[]{new(1,0), new(1,1), new(2,1), new(2,2)},
