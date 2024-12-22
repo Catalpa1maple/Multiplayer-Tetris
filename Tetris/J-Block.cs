274b1b1c6ac0442c24d623c1e82dec97ccc3c49e
sembreakproduct <1155191897@link.cuhk.edu.hk>
2024-12-23 01:10:41 +0800

    ef

--- Tetris/J-Block.cs
+++ Tetris/J-Block.cs
@@ -3,7 +3,7 @@ namespace Tetris{
     public class JBlock : Block{
         // Defines the tile positions for the four rotation states of the "J" block.
         // Each array represents a rotation state, and the positions are relative to the block's offset.
-        private readonly Position[][] tiles = new Position[][]{
+        private readonly Position[][] Tiles => new Position[][]{
             new Position[] {new(0,0), new(1,0), new(1,1), new(1,2) },
             new Position[] {new(0,1), new(0,2), new(1,1), new(2,1) },
             new Position[] {new(1,0), new(1,1), new(1,2), new(2,2) },
