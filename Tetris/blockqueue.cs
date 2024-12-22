01aa1ff395cbd2fdc37da11ce2d54b1705c27e6c
sembreakproduct <1155191897@link.cuhk.edu.hk>
2024-12-23 03:10:20 +0800

    fes

--- Tetris/blockqueue.cs
+++ Tetris/blockqueue.cs
@@ -14,38 +14,32 @@ namespace Tetris
             new TBlock(),
             new ZBlock()
         };
+
         private readonly Random random = new Random();
-        private readonly Random randomWrapper;
-        private Block lastBlock;
+
         public Block NextBlock { get; private set; }
 
         public BlockQueue()
         {
-            randomWrapper = new Random(); 
             NextBlock = RandomBlock();
-            lastBlock = NextBlock;
         }
 
         private Block RandomBlock()
         {
-            int index = randomWrapper.Next(blocks.Length);
-            return blocks[index];
+            return blocks[random.Next(blocks.Length)];
         }
 
         public Block GetAndUpdate()
         {
-            Block blockToReturn = NextBlock;
+            Block block = NextBlock;
+
             do
             {
                 NextBlock = RandomBlock();
-                if (lastBlock != null && NextBlock.Id == lastBlock.Id)
-                {
-                    continue; 
-                }
             }
-            while (blockToReturn.Id == NextBlock.Id);
-            lastBlock = NextBlock;
-            return blockToReturn;
+            while (block.Id == NextBlock.Id);
+
+            return block;
         }
     }
 }
