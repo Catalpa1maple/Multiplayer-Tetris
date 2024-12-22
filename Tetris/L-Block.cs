3308d497c2bfb6b6a8d02aa4d34bc486aa4466ec
ClementChu001 <clementchu001@gmail.com>
2024-12-22 13:01:52 +0800

    updated typo

--- Tetris/L-Block.cs
+++ Tetris/L-Block.cs
@@ -1,6 +1,6 @@
 namespace Tetris{
     public class LBlock : Block{
-        private readonly Position[][] tiles = new Positiom[][]{
+        private readonly Position[][] tiles = new Position[][]{
             new Position[] {new(0,2), new(1,0), new(1,1), new(1,2) },
             new Position[] {new(0,1), new(1,1), new(2,1), new(2,2) },
             new Position[] {new(1,0), new(1,1), new(1,2), new(2,0) },
@@ -8,6 +8,6 @@ namespace Tetris{
         };
         public override int Id=> 3;
         protected override Position StartOffset=> new Position(0,3);
-        protected override Positiom[][] Tiles => tiles;
+        protected override Position[][] Tiles => tiles;
     }
 }
