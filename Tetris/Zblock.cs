06c71f6ad5e8537e9b1175e78cd4a80991c9adf9
sembreakproduct <1155191897@link.cuhk.edu.hk>
2024-12-22 23:30:11 +0800

    typo

--- Tetris/Zblock.cs
+++ Tetris/Zblock.cs
@@ -1,7 +1,7 @@
 namespace Tetris
 { 
     public class ZBlock: Block{
-        new Position[][] tiles = new Position[][]{
+        private readonly Position[][] tiles = new Position[][]{
             new Position[]{new(0,0), new(0,1), new(1,1), new(1,2)},
             new Position[]{new(0,2), new(1,1), new(1,2), new(2,1)},
             new Position[]{new(1,0), new(1,1), new(2,1), new(2,2)},
